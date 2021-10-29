2077a783d64b2e60ea6c6864914094bbd6404ddf
rpcedill <rpcedill@espol.edu.ec>
2021-10-29 11:05:17 -0500

    creacion clase carro

---
+++ Taller/src/main/java/com/mycompany/taller/Carro.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.taller;
+
+/**
+ *
+ * @author USER
+ */
+public class Carro {
+    private String marca;
+    private String placa;
+
+    public Carro(String marca, String placa) {
+        this.marca = marca;
+        this.placa = placa;
+    }
+    
+    
+}
